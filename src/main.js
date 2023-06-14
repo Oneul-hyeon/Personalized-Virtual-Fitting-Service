e60cc966bdd65f00720a5d778621c29cefaf5c78
S686 <ath0117@naver.com>
2023-06-14 18:04:36 +0900

    :wrench: 수정: 리액트 앱 생성, 패키지 수정

--- src/main.js
+++ src/main.js
@@ -1,1 +1,23 @@
 // @ts-check
+
+const exp = require('constants')
+const express = require('express')
+
+const path = require('path')
+
+const app = express()
+
+app.use(express.json())
+var cors = require('cors')
+app.use(cors())
+
+app.listen(3000, () => {
+  console.log('App Listening on port 3000')
+})
+app.use(express.static(path.join(__dirname, '../style/build')))
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../style/build/index.html'))
+})
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../style/build/index.html'))
+})
