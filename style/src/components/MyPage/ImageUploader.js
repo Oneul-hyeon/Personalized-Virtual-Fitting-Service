e7cf831b6533b53f9edc3aae20e5ca04d8da742c
Paruru <paruru@gmail.com>
2023-07-05 06:47:53 +0900

    ✨ Feat: 추가 프로필 이미지 수정

---
+++ style/src/components/MyPage/ImageUploader.js
@@ -0,0 +1,70 @@
+import { useState, useRef } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { solid, regular } from '@fortawesome/fontawesome-svg-core/import.macro'
+import './ImageUploader.css'
+
+//Single Image Upload
+
+function ImageUploader({ onChange }) {
+  const [uploadedImage, setUploadedImage] = useState(null)
+  const inputRef = useRef()
+  const allowedExtensions = ['png', 'jpg', 'jpeg', 'bmp']
+
+  const handleFileChange = (e) => {
+    const file = e.target.files[0]
+    if (file && file.type.startsWith('image/')) {
+      const extension = file.name.split('.').pop()?.toLowerCase()
+      if (!allowedExtensions.includes(extension)) {
+        alert('Invalid file extension. Only images are allowed.')
+        return
+      }
+
+      const reader = new FileReader()
+      reader.onloadend = (e) => {
+        const result = e.target.result
+        if (result) {
+          setUploadedImage(result)
+          // onChange(file)
+        }
+      }
+      reader.readAsDataURL(file)
+
+      if (typeof onChange === 'function') {
+        onChange(file)
+      }
+    }
+  }
+
+  const handleClick = () => {
+    inputRef.current.click()
+  }
+
+  const handleDragOver = (e) => {
+    e.preventDefault()
+  }
+
+  return (
+    <div className="ImageUploader">
+      <div className="placeholder-text">
+        <div
+          className="image-container"
+          onClick={handleClick}
+          onDragOver={handleDragOver}
+        >
+          {/* <button className="image-add-button" onClick={handleClick}> */}
+          <FontAwesomeIcon icon={solid('camera')} />
+          {/* </button> */}
+        </div>
+      </div>
+      <input
+        type="file"
+        accept="image/*"
+        ref={inputRef}
+        onChange={handleFileChange}
+        style={{ display: 'none' }}
+      />
+    </div>
+  )
+}
+
+export default ImageUploader
