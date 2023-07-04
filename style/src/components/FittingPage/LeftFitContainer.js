d5588c46ea94f3322ff643dba9d154a80e3bb938
Ruru <adskus3412@gmail.com>
2023-07-05 08:48:04 +0900

    ✨ Feat: 추가 마이페이지 초기 이미지
    - 초기 이미지 서버에서 로딩하도록 변경
    - "이미지 로딩 중" 이미지 추가(아주 뎁악이에요!)
    - 코드 리팩토링 : 프로필 이미지 저장 함수 따로 분리
    - 사진 업로드 버튼 위치 이상한 부분 수정

--- style/src/components/FittingPage/LeftFitContainer.js
+++ style/src/components/FittingPage/LeftFitContainer.js
@@ -1,12 +1,10 @@
 import React, { useEffect, useState } from 'react'
 
-import CardButton from './CardButton'
 import ButtonBar from './ButtonBar'
 
 import styles from './FittingPage.module.css'
-import axios from 'axios'
-import { API_URL } from '../../api/apiConfig'
 import 'react-responsive-carousel/lib/styles/carousel.min.css'
+import getProfileImage from '../common/getProfileImage'
 
 function LeftFitContainer({
   setErrorCode,
@@ -18,25 +16,7 @@ function LeftFitContainer({
   const [image, setImage] = useState('')
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const user = JSON.parse(localStorage.getItem('user'))
-        if (!user || !user._id) {
-          console.error('User ID not found')
-          return
-        }
-        const userId = user._id
-        const response = await axios.get(`${API_URL}/userinfo/api/userimage`, {
-          params: {
-            userId: userId,
-          },
-        })
-        setImage(response.data.image)
-      } catch (error) {
-        console.error('Error fetching user data:', error)
-      }
-    }
-    fetchData()
+    getProfileImage(setImage)
   }, [])
 
   const defaultImageUrl = image
